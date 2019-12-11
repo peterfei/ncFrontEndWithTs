dd6b1764d6806df67c29e1a29b920e0f391cb7d1
peterfei <peterfeispace@gmail.com>
2019-12-11 14:07:28 +0800

    [Feature]加入element-ui

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+import { Table, TableColumn, Button } from "element-ui";
+
+Vue.use(Table);
+Vue.use(TableColumn);
+Vue.use(Button);
 Vue.config.productionTip = false;
 
 new Vue({
