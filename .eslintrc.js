09ea02dd7b577599001f1a7881da2a53d8511da8
peterfei <peterfeispace@gmail.com>
2019-12-17 11:31:05 +0800

    [Chore]修改eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'vue/script-indent': [
-            'error',
+            '',
             4,
             {
                 baseIndent: 1,
