38f32563603022ec30a2e966bf77caece8ce92f5
hqanian0@163.com <hqanian0@163.com>
2019-12-19 09:28:45 +0800

    [Fix]:去掉了全局textalign center

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,45 @@
 <template>
-    <div id="app">
-        <router-view />
-    </div>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 <script>
 export default {
-    name: 'app',
-    components: {
-        // AppHeader,
-    },
+  name: 'app',
+  components: {
+    // AppHeader,
+  }
 }
 </script>
 <style lang="scss">
 #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #212d3a;
 }
 
 #nav {
-    padding: 30px;
+  padding: 30px;
 
-    a {
-        font-weight: bold;
-        color: #2c3e50;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
 
-        &.router-link-exact-active {
-            color: #42b983;
-        }
+    &.router-link-exact-active {
+      color: #42b983;
     }
+  }
 }
 
 /* 页面过渡动画 */
 .router-fade-enter-active,
 .router-fade-leave-active {
-    transition: opacity 0.3s;
+  transition: opacity 0.3s;
 }
 
 .router-fade-enter,
 .router-fade-leave-active {
-    opacity: 0;
+  opacity: 0;
 }
 </style>
