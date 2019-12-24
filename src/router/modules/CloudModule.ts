b4f731089401214b69a7ba85a76c3aa0dce267ea
peterfei <peterfeispace@gmail.com>
2019-12-24 11:50:37 +0800

    [Fix]修复route cloudModule

--- src/router/modules/CloudModule.ts
+++ src/router/modules/CloudModule.ts
@@ -1,7 +1,7 @@
 // import CourseEvaluation from '@/views/cloud/cloudDetail/CourseEvaluation.vue';
 import Layout from '@/views/layout/LayoutPage.vue'
 import CloudList from '@/views/cloud/cloudList/CloudList.vue'
-import CloudDetail from '@/views/cloud/cloudDetail/index.vue';
+import CloudDetail from '@/views/cloud/cloudDetail/index.vue'
 
 // import CourseIntroduction from '@/views/cloud/cloudDetail/CourseIntroduction.vue';
 // import LearningSyllabus from '@/views/cloud/cloudDetail/LearningSyllabus.vue';
@@ -17,44 +17,44 @@ const CloudModule = {
       path: '',
       name: 'cloud',
       component: CloudList
-    },
-    {
-      path: 'clouddetail/:id/packages/:course_id',
-      name: 'CloudDetail',
-      component: CloudDetail,
-      // children: [
-      //   {
-      //     path: '',
-      //     name: 'CourseIntroduction',
-      //     component: CourseIntroduction,
-      //   },
-        // {
-        //   path: 'introduction',
-        //   name: 'CourseIntroduction',
-        //   component: CourseIntroduction,
-        // },
-        // {
-        //   path: 'syllabus',
-        //   name: 'LearningSyllabus',
-        //   component: LearningSyllabus,
-        // },
-        // {
-        //   path: 'notice',
-        //   name: 'CourseNotice',
-        //   component: CourseNotice,
-        // },
-        // {
-        //   path: 'rules',
-        //   name: 'ScoringRules',
-        //   component: ScoringRules,
-        // },
-        // {
-        //   path: 'evaluate',
-        //   name: 'CourseEvaluation',
-        //   component: CourseEvaluation,
-        // },
-      // ],
-    },
+    }
+    //{
+    //  path: 'clouddetail/:id/packages/:course_id',
+    //  name: 'CloudDetail',
+    //  component: CloudDetail
+    //  // children: [
+    //  //   {
+    //  //     path: '',
+    //  //     name: 'CourseIntroduction',
+    //  //     component: CourseIntroduction,
+    //  //   },
+    //  // {
+    //  //   path: 'introduction',
+    //  //   name: 'CourseIntroduction',
+    //  //   component: CourseIntroduction,
+    //  // },
+    //  // {
+    //  //   path: 'syllabus',
+    //  //   name: 'LearningSyllabus',
+    //  //   component: LearningSyllabus,
+    //  // },
+    //  // {
+    //  //   path: 'notice',
+    //  //   name: 'CourseNotice',
+    //  //   component: CourseNotice,
+    //  // },
+    //  // {
+    //  //   path: 'rules',
+    //  //   name: 'ScoringRules',
+    //  //   component: ScoringRules,
+    //  // },
+    //  // {
+    //  //   path: 'evaluate',
+    //  //   name: 'CourseEvaluation',
+    //  //   component: CourseEvaluation,
+    //  // },
+    //  // ],
+    //}
   ]
 }
 export default { CloudModule }
