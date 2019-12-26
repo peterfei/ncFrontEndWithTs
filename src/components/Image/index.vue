84b5fe8b0de42ce8f1f866631d7be2812a8a21e8
bmxy <41118318@qq.com>
2019-12-26 16:20:38 +0800

    [Feature]为IbdImage组件增加，width和height属性，不需再通过style设置

--- src/components/Image/index.vue
+++ src/components/Image/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <el-image :src="cSrc" v-bind="$attrs" v-on="$listeners"></el-image>
+  <el-image
+    :src="cSrc"
+    v-bind="$attrs"
+    v-on="$listeners"
+    :style="cStyle"
+  ></el-image>
 </template>
 
 <script lang="ts">
@@ -9,7 +14,23 @@ import { Component, Vue, Prop, Emit } from 'vue-property-decorator'
   name: 'IbdImage'
 })
 export default class Image extends Vue {
+  @Prop({ type: [String, Number] }) width: string | number
+  @Prop({ type: [String, Number] }) height: string | number
   @Prop({ type: String, default: '' }) src: string
+
+  get cStyle() {
+    if (!this.width && !this.height) {
+      return {}
+    }
+    const reg = /^\d+$/
+    const width = reg.test(this.width.toString())
+      ? this.width + 'px'
+      : this.width
+    const height = reg.test(this.height.toString())
+      ? this.height + 'px'
+      : this.height
+    return { width, height }
+  }
 
   // 后期根据图片域名及组件宽度和高度自适应添加压缩参数
   get cSrc() {
