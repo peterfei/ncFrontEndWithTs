21a0be4807703f003b512d11a712d55ac483838a
bmxy <41118318@qq.com>
2019-12-26 14:27:05 +0800

    [Feature]增加全局图片组件IbdImage

---
+++ src/components/Image/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-image :src="cSrc" v-bind="$attrs" v-on="$listeners"></el-image>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Emit } from 'vue-property-decorator'
+
+@Component({
+  name: 'IbdImage'
+})
+export default class Image extends Vue {
+  @Prop({ type: String, default: '' }) src: string
+
+  // 后期根据图片域名及组件宽度和高度自适应添加压缩参数
+  get cSrc() {
+    return this.src
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
