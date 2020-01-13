ac365c58bc5a424b01a751e428f6e68f78842828
peterfei <peterfeispace@gmail.com>
2020-01-13 11:16:36 +0800

    [Feature]云课后台引导页面

--- src/router/modules/BackendCloudModule.ts
+++ src/router/modules/BackendCloudModule.ts
@@ -1,6 +1,8 @@
 import Layout from '@/views/layout/LayoutPage.vue'
 import BackendCloud from '@/views/backendCloud/index.vue'
 import BackendCloudGuide from '@/views/backendCloud/guideCloud.vue'
+import Guide from '@/views/backendCloud/components/guide.vue'
+
 // import CloudDetail from '@/views/cloud/cloudDetail/index.vue'
 
 const BackendCloudModule = {
@@ -16,7 +18,13 @@ const BackendCloudModule = {
     {
       path: 'guide',
       name: 'backend_cloud_guide',
-      component: BackendCloudGuide
+      component: BackendCloudGuide,
+      children: [
+        {
+          path: '/',
+          component: Guide
+        }
+      ]
     }
   ]
 }
