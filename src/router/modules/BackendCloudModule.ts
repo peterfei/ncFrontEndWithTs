5e28b529eeffab51a4ab4653aca4c7386b759ea0
peterfei <peterfeispace@gmail.com>
2020-01-13 17:13:35 +0800

    [Style]修改云课后台引导页样式

--- src/router/modules/BackendCloudModule.ts
+++ src/router/modules/BackendCloudModule.ts
@@ -2,6 +2,7 @@ import Layout from '@/views/layout/LayoutPage.vue'
 import BackendCloud from '@/views/backendCloud/index.vue'
 import BackendCloudGuide from '@/views/backendCloud/guideCloud.vue'
 import Guide from '@/views/backendCloud/components/guide.vue'
+import Teaching from '@/views/backendCloud/components/teaching.vue'
 
 // import CloudDetail from '@/views/cloud/cloudDetail/index.vue'
 
@@ -12,17 +13,22 @@ const BackendCloudModule = {
   children: [
     {
       path: '/',
-      name: 'backend_cloud',
+      // name: 'backend_cloud',
       component: BackendCloud
     },
     {
       path: 'guide',
-      name: 'backend_cloud_guide',
+      // name: 'backend_cloud_guide',
       component: BackendCloudGuide,
       children: [
         {
           path: '/',
           component: Guide
+        },
+        {
+          path: 'teaching',
+          name: 'teaching',
+          component: Teaching
         }
       ]
     }
