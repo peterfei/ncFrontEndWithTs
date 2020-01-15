cc114e4ff60d37ca2fcda568f3cf2c0336edae83
bmxy <41118318@qq.com>
2020-01-15 13:06:08 +0800

    [Feature]增加了一个开发环境中的测试页面及路由，完成后会移除该页面

---
+++ src/views/Test.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="i-test">
+    <ibd-upload multiple>
+      <el-button>点击上传</el-button>
+      <div slot="tip">
+        hahha
+      </div>
+    </ibd-upload>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'IbdTest',
+  components: {
+    IbdUpload: () => import('../components/Upload/index.vue')
+  }
+})
+export default class IbdTest extends Vue {
+  handlerUploader() {
+    console.log('123')
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
